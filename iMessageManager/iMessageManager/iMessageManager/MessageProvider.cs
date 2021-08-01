2d4dcfc4e996ff19d62e3b68373b03ecd23788b2
MrRiptide <mrriptidemc@gmail.com>
2021-08-01 15:35:34 -0400

    fix gitignore

---
+++ iMessageManager/iMessageManager/iMessageManager/MessageProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iMessageManager
+{
+    class MessageProvider : List<Message>
+    {
+        public MessageProvider(string clause)
+        {
+            List<>
+        }
+
+        override public 
+    }
+}
