93fef2bfa9891e4b63505be1a97a91aa08c768dd
MrRiptide <mrriptidemc@gmail.com>
2021-09-03 16:41:28 -0400

    upgrading search functionality
    not complete

---
+++ iMessageManager/iMessageManager/iMessageManager/SearchOptionsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace iMessageManager
+{
+    /// <summary>
+    /// Interaction logic for SearchOptionsWindow.xaml
+    /// </summary>
+    public partial class SearchOptionsWindow : Window
+    {
+        public SearchOptionsWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
